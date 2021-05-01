0497974a754f911059e519542e72183a68fe5dbe
Benben <z2266109@gmail.com>
2021-05-01 10:36:39 +0800

    update chellange

--- homeworks/week3/hw7-chellange-LIOJ-1053.js
+++ homeworks/week3/hw7-chellange-LIOJ-1053.js
@@ -1,8 +1,8 @@
+// 過程可參考 : https://hackmd.io/VEjiIM_SQjO-OnQ2YibtmQ?view
 // eslint-disable-next-line
 function solve(lines) {
   const maze = []
   const queue = []
-  const path = []
 
   // 輸入迷宮 maze
   for (let i = 1; i < lines.length; i++) {
@@ -19,18 +19,15 @@ function solve(lines) {
     const [i, j] = queue[0]
 
     // 終止條件 : 當 [i, j] 等於右下角出口時
-    if (i === maze.length - 1 && j === maze[0].length - 1) {
-      break
-    }
+    if (i === maze.length - 1 && j === maze[0].length - 1) break
 
-    // 如果未尋訪過，將 maze[i][j] 設為 i + j 步數
+    // 如果未尋訪過，將 maze[i][j] 設為 i + j 步數，表示尋訪
     if (maze[i][j] === '.') {
       maze[i][j] = `${i + j}`
     }
 
-    // 將尋訪過的點從 Queue 取出，放入 path 路徑
+    // 將尋訪過的點從 Queue 取出
     queue.shift()
-    path.push([i, j])
 
     searchWay(i - 1, j, maze, maze[i][j], queue)
     searchWay(i + 1, j, maze, maze[i][j], queue)
@@ -38,31 +35,21 @@ function solve(lines) {
     searchWay(i, j + 1, maze, maze[i][j], queue)
 
     // degbug 用
-    // console.log(maze)// 打開可以看到整個路徑
+    // console.log(maze)    // 打開可以看到整個路徑，很療癒XD
     // console.log(queue)
-    // console.log(path)
   }
   // 輸出迷宮終點的步數
   console.log(maze[maze.length - 1][maze[0].length - 1])
 }
 
-// 輔助 function 檢查 [i, j] 是否有在 arr 裡
-function isInArr(arr, i, j) {
-  for (let n = 0; n < arr.length; n++) {
-    if (arr[n][0] === i && arr[n][1] === j) {
-      return true
-    }
-  }
-  return false
-}
-
-// 如果當前位置 [i, j] 有路可以走 ( 上、下、左、右 ) 並且不在 queue 裡面就加進 queue 並加入步數
+// 輔助 function searchWay
+// 如果當前位置 [i, j] 有路可以走 ( 上、下、左、右 ) 就加進 queue 並加入步數
 function searchWay(x, y, maze, step, queue) {
   // 如果超出邊際值返回
   if (x < 0 || x > maze.length - 1) return
   if (y < 0 || y > maze[0].length - 1) return
 
-  if (maze[x][y] === '.' && !isInArr(queue, x, y)) {
+  if (maze[x][y] === '.') {
     queue.push([x, y])
     maze[x][y] = `${Number(step) + 1}`
   }
