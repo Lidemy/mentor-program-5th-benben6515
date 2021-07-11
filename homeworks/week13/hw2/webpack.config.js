7a9ec4bea7684f33e4d7788ed1751bc8b47fb2b5
Benben <z2266109@gmail.com>
2021-07-11 22:31:38 +0800

    fix space

--- homeworks/week13/hw2/webpack.config.js
+++ homeworks/week13/hw2/webpack.config.js
@@ -2,11 +2,11 @@ const path = require('path')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-	mode: 'development',
-	entry: './src/index.js',
-	output: {
-		filename: 'main.js',
-		path: path.resolve(__dirname, 'dist'),
-		library: 'commentPlugin'
-	}
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: 'commentPlugin'
+  }
 }
